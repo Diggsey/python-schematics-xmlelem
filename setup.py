31969310b8e8a7dcca83b854ea049419ce62f11f
Diggory Blake <diggsey@googlemail.com>
2021-11-04 15:59:23 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='schematics_xmlelem',
     version='0.1',
-    py_modules=['schematics_xmlelem'],
+    packages=find_packages(),
     license='MIT License',
     long_description='Define schematics-like models over an XML-like structure',
 )
